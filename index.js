911a4e30e94282798bf1a665f15eaf5b8c074905
mapsam <matthews.sam@gmail.com>
2016-04-03 16:12:32 -0400

    comments

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/* Takes a string and exclaims it.
+ * @param {string} any old string
+ * @returns {string} string with an exclamation point
+ * @example
+ * var exclaim = require('exclaim');
+ * var exciting = exclaim('hi');
+ * console.log(exciting); // hi!
+ */
 module.exports = function(string) {
   return string + '!';
 };
